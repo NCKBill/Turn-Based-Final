f937e6bf1fb4c71f830b0afd126af61d458be8a3
Khanh Phung <kphunggia@gmail.com>
2026-05-25 23:58:18 +0700

    fix action

--- src/main/java/Unit/Mage.java
+++ src/main/java/Unit/Mage.java
@@ -6,6 +6,8 @@ import Controller.Controller;
 public class Mage extends Unit {
     public Mage(boolean friendly, Controller unitController) {
         super("Mage", 60, 2, 10, 5, 5, 5, friendly, unitController);
+        this.setMaxMP(3);
+        this.setMP(3);
         this.addAction(new MagicAttack("Damage", "Fireball", 3, 10, 3, false));
     }
 }
