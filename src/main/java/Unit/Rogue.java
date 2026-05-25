701310f5e27c379654d81bebfc4ff20c49a6adaf
Ky Canh Nguyen <nguyencanhky1807@gmail.com>
2026-05-25 19:23:20 +0700

    Balance change.
    Signed-off-by: Ky Canh Nguyen <nguyencanhky1807@gmail.com>

--- src/main/java/Unit/Rogue.java
+++ src/main/java/Unit/Rogue.java
@@ -8,8 +8,8 @@ public class Rogue extends Unit{
     public Rogue(boolean friendly, Controller unitController) {
         // super(name, HP, STR, POW, DEF, MAG DEF, INIT, friendly, controller)
         super("Rogue", 70, 30, 0, 5, 5, 5, friendly, unitController);
-        this.setMP(4);
-        this.setMaxMP(4);
+        this.setMP(5);
+        this.setMaxMP(5);
         this.addAction(new PhysicalAttack("Damage", "Assassinate", 6, 5, 1, false));
         this.addAction(new DefAttack("Damage", "Dagger Spray", 6, 3, 3, false));
     }
