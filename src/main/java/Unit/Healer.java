dff2a98a237428c0d6ff93496fd52bf7f831328e
Khanh Phung <kphunggia@gmail.com>
2026-05-26 01:20:37 +0700

    balance

--- src/main/java/Unit/Healer.java
+++ src/main/java/Unit/Healer.java
@@ -6,10 +6,10 @@ import Controller.Controller;
 
 public class Healer extends Unit {
     public Healer(boolean friendly, Controller unitController) {
-        super("Healer", 60, 2, 20, 4, 15, 5, friendly, unitController);
+        super("Healer", 60, 2, 15, 4, 15, 5, friendly, unitController);
         this.setMaxMP(3);
         this.setMP(3);
         this.addAction(new Heal("Buff", "Divine Heal", 3, 5, 3, true));
-        this.addAction(new MagicAttack("Damage", "Holy Nova", 2, 2, 3, false));
+        this.addAction(new MagicAttack("Damage", "Holy Nova", 2, 5, 3, false));
     }
 }
--- src/main/java/Unit/Mage.java
+++ src/main/java/Unit/Mage.java
@@ -8,6 +8,7 @@ public class Mage extends Unit {
         super("Mage", 60, 2, 10, 5, 5, 5, friendly, unitController);
         this.setMaxMP(3);
         this.setMP(3);
-        this.addAction(new MagicAttack("Damage", "Fireball", 3, 10, 3, false));
+        this.addAction(new MagicAttack("Damage", "Fireball", 3, 15, 1, false));
+        this.addAction(new MagicAttack("Damage", "Snowball", 3, 10, 3, false));
     }
 }
